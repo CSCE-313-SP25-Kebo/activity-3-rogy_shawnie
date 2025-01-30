c904603971ede43c3c6925e4dfd7c8fa4556a9c9
Shassam-Tamu <schin@tamu.edu>
2025-01-30 15:32:52 -0600

    testing push

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <sys/wait.h>
 
 using namespace std;
-
+//test push
 int main(int argc, char *argv[])
 {
     int option = 0; // default option: execute the command ls -l and terminate normally
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
     {
         cout << "Hello from the child process!" << endl;
         /* TODO: PRINT THE PARENT PID value: "The parent process ID is $ID" */
-        cout << "The parent process ID is " << getpid() << endl;
+        cout << "The parent process ID is " << getppid() << endl;
         if (option % 2 == 0) // if the option number is even, execute the command ls -l and terminate normally
         {
             std::cout << "The child process will execute the command: ls -l after 6 seconds" << std::endl;
@@ -57,7 +57,7 @@ int main(int argc, char *argv[])
         cout << "\nHello from the parent process!" << endl;
 
         /* TODO: PRINT THE CHILD PID value: "The child process ID is $ID" */
-        cout << "The child process ID is" << newproc  << endl;
+        cout << "The child process ID is " << newproc  << endl;
         /* TODO: PRINT THE EXIT STATUS OF THE CHILD PROCESS BASED waitpid().
         MAKE SURE TO PASS BY REFERENCE THE STATUS VARIABLE TO THE SECOND PARAMETER OF waitpid()
         IF WIFEXITED, PRINT THE MESSAGE "The child process exited normally" WITH ENDLINE
